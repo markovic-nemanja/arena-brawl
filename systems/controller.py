e98bb72b54e3d455504e733bcf808a8919656251
Nemanja Marković <nemanja.contactme@gmail.com>
2026-04-21 19:11:56 +0200

    Add keyboard and AI controller classes

--- systems/controller.py
+++ systems/controller.py
@@ -1,3 +1,33 @@
 import pygame
 import settings as S
 import math
+
+class KeyboardController:
+    def __init__(self, up, down, left, right):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
+        
+    def get_movement(self, keys, player, opponent):
+        dx, dy = 0, 0
+        if keys[self.up]: dy-=1
+        if keys[self.down]: dy+=1
+        if keys[self.left]: dx-=1
+        if keys[self.right]: dx+=1
+        return dx, dy
+    
+    def get_action(self, keys, player, opponent):
+        return keys[pygame.K_SPACE]
+    
+class AIController:
+    def __init__(self):
+        self.attack_range = 300
+        
+    def get_movement(self, keys, player, opponent):
+        dx = opponent.x - player.x
+        dy = opponent.y - player.y
+        distance = math.hypot(dx, dy)
+        if distance == 0:
+            return 0, 0
+        return dx / distance, dy / distance
